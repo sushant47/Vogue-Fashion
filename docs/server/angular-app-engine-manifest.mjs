9e5e14c954311641eaf2c95574057815c0009334
Sushant Bilgi <sushantbilgi47@gmail.com>
2026-08-16 22:43:44 +0530

    fix: update base href and stylesheet link in index.server.html for correct resource loading

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/browser',
   allowedHosts: [],
   supportedLocales: {
   "en-US": ""
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,42 +2,42 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/browser/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/browser"
   },
   {
     "renderMode": 0,
-    "route": "/collection/*"
+    "route": "/browser/collection/*"
   },
   {
     "renderMode": 0,
-    "route": "/product/*"
+    "route": "/browser/product/*"
   },
   {
     "renderMode": 2,
-    "route": "/bag"
+    "route": "/browser/bag"
   },
   {
     "renderMode": 2,
-    "route": "/wishlist"
+    "route": "/browser/wishlist"
   },
   {
     "renderMode": 2,
-    "redirectTo": "/",
-    "route": "/**"
+    "redirectTo": "/browser",
+    "route": "/browser/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 32999, hash: 'deb1b4d292068ff45c32dbe6e131e7d937bc77d0ae5abee12340dca8b32d7eac', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 23343, hash: 'd404dae4cafbcaa90e50d71d9a1f6cb785f2e59b48b404649f9d1273de912261', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'wishlist/index.html': {size: 60094, hash: '29c5f8cde8ab32f1ae379ed80b80c13c725a47fd110b8c60b17d510613389a83', text: () => import('./assets-chunks/wishlist_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 80521, hash: 'e293ba0ba2f2449750a8622cac2a039213e8049b5aafe99264050291a8bae2f2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'bag/index.html': {size: 70476, hash: '4f0e447fdd4ebee6734d2c71a7f836a10939e97a252ac40338459fa19c3d7ee0', text: () => import('./assets-chunks/bag_index_html.mjs').then(m => m.default)},
-    'styles-XSQYLCXS.css': {size: 55262, hash: 'Ha8fV9S8+kA', text: () => import('./assets-chunks/styles-XSQYLCXS_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 33007, hash: '4a995f15b55fb5960adfbad09479a7bf826146fc4c39e9abea50e80417df564b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 23351, hash: '00d035547528170c12605fcf3ab4bc307f14aabeb18a7eae2c4d2f2a53df1012', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'wishlist/index.html': {size: 60198, hash: '0dcbac80bd08ffaf15e3dc9d8a93747f0fe78e41a9a06d270329e7d66e62025d', text: () => import('./assets-chunks/wishlist_index_html.mjs').then(m => m.default)},
+    'bag/index.html': {size: 70588, hash: '2887981b092bb990905782fd1fd8df18dbabcb0ac4bacbf9c1fca79c6429ecfe', text: () => import('./assets-chunks/bag_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 80697, hash: '3dd777aef9cbb9d984c5237d36ff58e7530b14b77f3b72fd8c7504eb810dce00', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-25UX4UMT.css': {size: 56966, hash: 'ojQG0gqMngY', text: () => import('./assets-chunks/styles-25UX4UMT_css.mjs').then(m => m.default)}
   },
 };
